2982b41695fe94046dc813f95a785ad6c61be820
Daniela Di Iorio <danielad@gmail.com>
2018-09-21 13:56:54 -0700

    lines for verse method works

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,5 +1,9 @@
 class Alouette
+
+  PARTS = ["la tete", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+
   def self.lines_for_verse(verse_num)
+    return PARTS[0..verse_num - 1]
   end
 
   def self.verse(verse_num)
