61bf41a2c2f9d9ff3abe762cc33a2417be1e3f0b
Daniela Di Iorio <danielad@gmail.com>
2018-09-21 14:25:34 -0700

    tests passing for verse and lines for verse

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,19 +1,26 @@
+require 'pry'
+
 class Alouette
 
-  PARTS = ["la tete", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+  PARTS = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
 
   def self.lines_for_verse(verse_num)
-    return PARTS[0..verse_num - 1]
+    lines = []
+    (verse_num + 1).times do |i|
+      lines << "Et #{PARTS[i]}!"
+    end
+    lines.reverse!
+    return lines
   end
 
   def self.verse(verse_num)
     parts = self.lines_for_verse(verse_num)
     verse = ""
 
-    2.times {verse << "Je te plumerai #{parts.last}.\n"}
+    2.times {verse << "Je te plumerai #{PARTS[verse_num]}.\n"}
 
-    parts.reverse_each do |part|
-      2.times {verse << "Et #{part}!\n"}
+    parts.each do |part|
+      2.times {verse << "#{part}\n"}
     end
 
     2.times {verse << "Alouette!\n"}
@@ -26,5 +33,3 @@ class Alouette
   def self.sing
   end
 end
-
-puts Alouette.verse(5)
